3832324d26221319a5968255baa4f65b71772540
EmpleadoED_24374666w <jferrer@yellowlab.es>
2021-01-14 10:41:34 +0100

    versión final con javadoc DNI 24374666w

--- src/empleadoED_24374666w/empleadoED_24374666w.java
+++ src/empleadoED_24374666w/empleadoED_24374666w.java
@@ -2,19 +2,37 @@
 package empleadoED_24374666w;
 
 /**
- *
- * @author jose
+ *Ejercicio práctico examen EVAL 1 ED
+ * 
+ * @author Jose Ferrer DNI 24374666W
+ * @vesion 1.0
  */
 public class EmpleadoED_24374666w {
 
     static final double FACTOR_HORA_EXTRA = 1.2;
     private String nombreCompleto, dni;
     private double salarioBasePorHora = 10.56;
-
+/**
+ *Constructor de tres parámetros
+ * 
+ * @param nombreCompleto nombre completo en mayúsculas
+ * @param dni documento nacional de identidad con letra y sin espacios
+ */
+    
+    
     public EmpleadoED_24374666w(String nombreCompleto, String dni) {
         this.nombreCompleto = nombreCompleto;
         this.dni = dni;
     }
+    
+ /**
+ *Calcula el salario bruto mensual en función del salario base por hora y las horas extras
+ *El precio por hora de la hora extra se determnina con la constante FACTOR_HORA_EXTRA 
+ * 
+ * @param horasExtra número de horas extra dedicadas redondeado a la baja (entero)
+ * 
+ * @return devuelve el salario bruto mensual en euros con dos decimales
+ */
 
     public double getSalarioEsteMes(int horasExtra) {
         double cantidadExtra, salarioFinal;
